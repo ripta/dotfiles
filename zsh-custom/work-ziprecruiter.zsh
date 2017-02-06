125390d9693ad12d17c6759b4bffe730f1b50a55
Ripta Pasay <ripta@users.noreply.github.com>
2017-02-06 15:19:09 -0800

    zsh-custom/work-ziprecruiter: repo locations and such

--- zsh-custom/work-ziprecruiter.zsh
+++ zsh-custom/work-ziprecruiter.zsh
@@ -1,3 +1,14 @@
 # ZipRecruiter
 export LOCAL_SANDBOX_USERNAME=ripta
 export STARTERVIEW=~/ziprecruiter
+
+export ZR_REPO="$HOME/Projects/git/ZipRecruiter"
+alias gozr="cd $ZR_REPO"
+
+# Infrastructure repository
+export INFRAWD="$HOME/Projects/git/ZipRecruiter/infrastructure"
+if [ -d "$INFRAWD/bin" ]
+then
+  export PATH="$INFRAWD/bin:$PATH"
+fi
+
