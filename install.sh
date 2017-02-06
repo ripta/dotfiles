ba03d9f9aeaa556079d3b9de156572deeb6fea41
Ripta Pasay <ripta@users.noreply.github.com>
2017-02-06 14:44:24 -0800

    install.sh: dotfiles installation script

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cd $HOME
+if [ ! -d "$HOME/.dotfiles/.git" ]
+then
+    git clone git@github.com:ripta/dotfiles.git ~/.dotfiles
+    cd ~/.dotfiles
+    git submodule init
+    git submodule update
+fi
+
+RCRC=~/.dotfiles/rcrc PATH=/home/ripta/.dotfiles/bin:$PATH rcup -v
+
