a3f77d629faedd6aae026ff5ee33195ce461571a
Ripta Pasay <ripta@users.noreply.github.com>
2017-02-17 14:41:20 -0800

    zsh-custom/work-ziprecruiter: better STARTERVIEW detection

--- zsh-custom/work-ziprecruiter.zsh
+++ zsh-custom/work-ziprecruiter.zsh
@@ -1,14 +1,33 @@
 # ZipRecruiter
-export LOCAL_SANDBOX_USERNAME=ripta
-export STARTERVIEW=~/ziprecruiter
-
-export ZR_REPO="$HOME/Projects/git/ZipRecruiter"
-alias gozr="cd $ZR_REPO"
-
-# Infrastructure repository
-export INFRAWD="$HOME/Projects/git/ZipRecruiter/infrastructure"
-if [ -d "$INFRAWD/bin" ]
+if [ -d "$HOME/ziprecruiter" ]
 then
-  export PATH="$INFRAWD/bin:$PATH"
+  export STARTERVIEW="$HOME/ziprecruiter"
+elif [ -d "/var/starterview" ]
+then
+  export STARTERVIEW="/var/starterview"
 fi
 
+if [ -d "$STARTERVIEW" -o -f "/etc/ziprecruiter.yml" ]
+then
+  echo "Welcome to ZipRecruiter!"
+else
+  return
+fi
+
+export LOCAL_SANDBOX_USERNAME=ripta
+export STARTERVIEW="$HOME/ziprecruiter"
+
+ZR_REPO="$HOME/Projects/git/ZipRecruiter"
+if [ -d "$ZR_REPO" ]
+then
+  export ZR_REPO
+  alias gozr="cd $ZR_REPO"
+fi
+
+# Infrastructure repository
+INFRAWD="$HOME/Projects/git/ZipRecruiter/infrastructure"
+if [ -d "$INFRAWD/bin" ]
+then
+  export INFRAWD
+  export PATH="$INFRAWD/bin:$PATH"
+fi
