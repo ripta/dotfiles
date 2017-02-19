83b519e3c79749bf70a379fd6375ee4059e4561f
Ripta Pasay <ripta@users.noreply.github.com>
2017-02-19 15:47:14 -0800

    zsh-custom/work-ziprecruiter: add more STARTERVIEW options

--- zsh-custom/work-ziprecruiter.zsh
+++ zsh-custom/work-ziprecruiter.zsh
@@ -5,24 +5,24 @@ then
 elif [ -d "/var/starterview" ]
 then
   export STARTERVIEW="/var/starterview"
+elif [ -d "$HOME/Projects/ziprecruiter" ]
+then
+  export STARTERVIEW="$HOME/Projects/ziprecruiter"
+elif [ -d "$HOME/Projects/zr/ziprecruiter" ]
+then
+  export STARTERVIEW="$HOME/Projects/zr/ziprecruiter"
 fi
 
 if [ -d "$STARTERVIEW" -o -f "/etc/ziprecruiter.yml" ]
 then
   echo "Welcome to ZipRecruiter!"
+  echo "STARTERVIEW=$STARTERVIEW"
+  alias gozr="cd $STARTERVIEW"
 else
   return
 fi
 
 export LOCAL_SANDBOX_USERNAME=ripta
-export STARTERVIEW="$HOME/ziprecruiter"
-
-ZR_REPO="$HOME/Projects/git/ZipRecruiter"
-if [ -d "$ZR_REPO" ]
-then
-  export ZR_REPO
-  alias gozr="cd $ZR_REPO"
-fi
 
 # Infrastructure repository
 INFRAWD="$HOME/Projects/git/ZipRecruiter/infrastructure"
