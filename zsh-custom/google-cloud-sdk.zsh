e1e868487a2f63b0a337f33e332abb2dc49ee7d2
Ripta Pasay <ripta@users.noreply.github.com>
2018-01-08 01:36:31 -0800

    zsh-custom/plugins: use system python for google-cloud-sdk

--- zsh-custom/google-cloud-sdk.zsh
+++ zsh-custom/google-cloud-sdk.zsh
@@ -3,4 +3,8 @@ if [[ -d ~/bin/google-cloud-sdk ]]
 then
     source ~/bin/google-cloud-sdk/path.zsh.inc
     source ~/bin/google-cloud-sdk/completion.zsh.inc
+
+    # google-cloud-sdk prioritizes brew-installed python over system python,
+    # which unfortunately doesn't work
+    export CLOUDSDK_PYTHON=python
 fi
