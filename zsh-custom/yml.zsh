95a7dbf51cddd7bdffb829a0862ac0ad97c8dd8d
Ripta Pasay <ripta@users.noreply.github.com>
2018-03-22 13:12:35 -0700

    zsh-custom: by default, stringify python objects into json

--- zsh-custom/yml.zsh
+++ zsh-custom/yml.zsh
@@ -1,5 +1,5 @@
 function y2j() {
-    python2.7 -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout)'
+    python2.7 -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, default=str)'
 }
 
 function yq() {
