033b9773c26012a08ad992daac559474e305b0ee
Ripta Pasay <ripta@users.noreply.github.com>
2018-04-06 14:01:43 -0700

    zsh-custom: add `tss` alias

--- zsh-custom/awk.zsh
+++ zsh-custom/awk.zsh
@@ -1,2 +1,2 @@
-alias ts='gawk '"'"'{ print strftime("%Y-%m-%dT%H:%M:%S%z"), $0; fflush(); }'"'"
-alias tss='gawk '"'"'{ print strftime("%H:%M:%S%z"), $0; fflush(); }'"'"
+alias ts='gawk '"'"'{ print strftime("%Y-%m-%dT%H:%M:%S%z |"), $0; fflush(); }'"'"
+alias tss='gawk '"'"'{ print strftime("%H:%M:%S |"), $0; fflush(); }'"'"
