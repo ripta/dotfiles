e092954abddd0c1e9dc81665a49aae831bbbfe88
Ripta Pasay <ripta@users.noreply.github.com>
2018-06-02 19:40:20 -0700

    zsh-custom/yml: json-to-yaml conversion

--- zsh-custom/yml.zsh
+++ zsh-custom/yml.zsh
@@ -1,3 +1,7 @@
+function j2y() {
+    python2.7 -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False, encoding=None)'
+}
+
 function y2j() {
     python2.7 -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, default=str)'
 }
