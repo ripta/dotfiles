4ef38f194c59958a5598e055554c89167afb745f
Ripta Pasay <ripta@users.noreply.github.com>
2019-06-23 19:07:00 -0700

    zsh-custom/yml: safe-load yaml

--- zsh-custom/yml.zsh
+++ zsh-custom/yml.zsh
@@ -3,7 +3,7 @@ function j2y() {
 }
 
 function y2j() {
-    python2.7 -c 'import sys, yaml, json; json.dump(yaml.load(sys.stdin), sys.stdout, default=str)'
+    python2.7 -c 'import sys, yaml, json; json.dump(yaml.safe_load(sys.stdin), sys.stdout, default=str)'
 }
 
 function yq() {
