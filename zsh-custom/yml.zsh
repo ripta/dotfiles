52bf668038cbba357d7395f54ebf00df3e5f8790
Ripta Pasay <ripta@users.noreply.github.com>
2019-06-23 19:07:25 -0700

    zsh-custom/yml: upgrade to python3

--- zsh-custom/yml.zsh
+++ zsh-custom/yml.zsh
@@ -1,9 +1,9 @@
 function j2y() {
-    python2.7 -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False, encoding=None)'
+    python3 -c 'import sys, yaml, json; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False, encoding=None)'
 }
 
 function y2j() {
-    python2.7 -c 'import sys, yaml, json; json.dump(yaml.safe_load(sys.stdin), sys.stdout, default=str)'
+    python3 -c 'import sys, yaml, json; json.dump(yaml.safe_load(sys.stdin), sys.stdout, default=str)'
 }
 
 function yq() {
