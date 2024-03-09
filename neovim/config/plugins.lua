f0e7471b27b52074480dbf19f72d2786ae5fe225
Ripta Pasay <ripta@users.noreply.github.com>
2024-03-09 00:25:30 -0800

    neovim: move LspAttach listener next to the nvim-lspconfig plugin setup

--- neovim/config/plugins.lua
+++ neovim/config/plugins.lua
@@ -146,6 +146,33 @@ local function init(use)
       cfg.terraformls.setup {}
       cfg.tsserver.setup {}
       cfg.yamlls.setup {}
+
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+        callback = function(evt)
+          vim.bo[evt.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+          local opts = { buffer = evt.buf }
+          vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+          vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+          vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+          vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+          vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+          vim.keymap.set('n', '<leader>wl', function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+          end, opts)
+          vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+          vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+          vim.keymap.set({ 'n', 'v' }, '<leader>ca', vim.lsp.buf.code_action, opts)
+          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', '<leader>f', function()
+            vim.lsp.buf.format { async = true }
+          end, opts)
+        end,
+      })
+
     end,
   }
 
