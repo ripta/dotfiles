e0568e45bb12cdfe568cbe30b7ed5375a22b4a22
Ripta Pasay <ripta@users.noreply.github.com>
2025-03-19 00:49:45 -0700

    wezterm: hide new-tab button

--- wezterm.lua
+++ wezterm.lua
@@ -166,4 +166,6 @@ config.keys = {
 config.send_composed_key_when_left_alt_is_pressed = true
 config.send_composed_key_when_right_alt_is_pressed = false
 
+config.show_new_tab_button_in_tab_bar = false
+
 return config
