db71dbad739faa2f09e370cd4dc2e379abd2aeca
Ripta Pasay <ripta@users.noreply.github.com>
2025-05-10 19:14:37 -0700

    zsh-custom: unset GOROOT but keep adding GOPATH/bin to my PATH

--- zsh-custom/go.zsh
+++ zsh-custom/go.zsh
@@ -1,13 +1,4 @@
-# Add GOPATH for golang
-export GOPATH=$HOME/Projects/gocode
-
 if which go > /dev/null
 then
-    if [[ ! -d "$GOPATH" ]]
-    then
-        mkdir -p "$GOPATH/bin"
-    fi
-
-    export GOROOT=`go env GOROOT`
-    export PATH=$GOPATH/bin:$PATH
+    export PATH=$(go env GOPATH)/bin:$PATH
 fi
